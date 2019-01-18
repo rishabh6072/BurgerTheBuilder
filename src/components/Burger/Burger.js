5af037ed8fd21bb694141a6ae4bebfdd54852912
Rishabh jain <rishabh.jain@affle.com>
2019-01-18 18:02:22 +0530

    create burger by passing static data

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import classes from './Burger.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const burger = (props) => {
+    console.log('Object.keys(props.ingredients)', Object.keys(props.ingredients));
+    let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
+        return [...Array(props.ingredients[igKey])].map((_, i) => {
+            return <BurgerIngredient key={igKey + i} type={igKey} /> ;
+        })
+    }).reduce((prev, curr) => {
+        return prev.concat(curr);
+    }, []);
+    if( transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!!</p>
+    }
+    console.log('transformedIngredients', transformedIngredients);
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );
+}
+
+export default burger;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react';
 
+import Burger from '../../components/Burger/Burger';
+
 class BurgerBuilder extends Component {
+
+    state = {
+        ingredients: {
+            cheese: 1,
+            salad: 1,
+            bacon: 1,
+            meat: 1,
+        }
+    }
+
     render() {
         return (
             <>
-                <div>Burger</div>
+                <Burger ingredients={this.state.ingredients} />
                 <div>Build Controls</div>
             </>
         );
