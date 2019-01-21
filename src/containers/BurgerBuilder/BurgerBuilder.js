8ea42464cf329532eb0cf462e607966676841fc7
Rishabh jain <rishabh.jain@affle.com>
2019-01-21 14:35:06 +0530

    L133

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -78,6 +78,14 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: true });
     }
 
+    purchaseCancelHandler = () => {
+        this.setState({ purchasing: false });
+    }
+
+    purchaseContinueHandler = () => {
+        alert('Cannot continue at this moment!!');
+    }
+
     render() {
         const disabledInfo = {
             ...this.state.ingredients
@@ -88,8 +96,13 @@ class BurgerBuilder extends Component {
         // {salad: true, meat: false, bacon: false, cheese: true}
         return (
             <>
-                <Modal show={this.state.purchasing}>
-                    <OrderSummary ingredients={this.state.ingredients} />
+                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                    <OrderSummary
+                        ingredients={this.state.ingredients}
+                        purchaseCancelled={this.purchaseCancelHandler}
+                        purchaseContinued={this.purchaseContinueHandler}
+                        price={this.state.totalPrice}
+                    />
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls
